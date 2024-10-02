7c2ae8863db938854b6c8656908aed517cd9d0e8
gnslgns0 <gnslgns0@gmail.com>
2024-10-02 13:44:28 +0900

    작업본 커밋

--- src/routes/Gallery/index.tsx
+++ src/routes/Gallery/index.tsx
@@ -8,8 +8,29 @@ import image01 from 'assets/images/IMG_1.jpg'
 import image02 from 'assets/images/IMG_2.jpg'
 import image03 from 'assets/images/IMG_3.jpg'
 import image04 from 'assets/images/IMG_4.jpg'
+import image05 from 'assets/images/IMG_5.jpg'
+import image06 from 'assets/images/IMG_6.jpg'
+import image07 from 'assets/images/IMG_7.jpg'
+import image08 from 'assets/images/IMG_8.jpg'
+import image09 from 'assets/images/IMG_9.jpg'
+import image10 from 'assets/images/IMG_10.jpg'
+import image11 from 'assets/images/IMG_11.jpg'
+import image12 from 'assets/images/IMG_12.jpg'
 
-const imageList = [image01, image02, image03, image04]
+const imageList = [
+  image01,
+  image02,
+  image03,
+  image04,
+  image05,
+  image06,
+  image07,
+  image08,
+  image09,
+  image10,
+  image11,
+  image12,
+]
 
 const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
